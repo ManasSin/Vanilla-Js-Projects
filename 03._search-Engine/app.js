dc44f3a638b113b980023ff11fe6e31194942688
ManasSin <maltisadan12@gmail.com>
2023-02-09 15:22:34 +0530

    Completed | Search product using Vanilla JS

--- 03._search-Engine/app.js
+++ 03._search-Engine/app.js
@@ -1,79 +1,79 @@
 const productDetails = [
   {
-    title: "Bahamas",
+    title: "Long T-shirt",
     info: "Mens t-shirt",
     imgSrc:
       "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
     price: "₹300",
   },
   {
-    title: "Bahamas pants",
+    title: "Mens pants",
     info: "Mens Pant",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1602293589930-45aad59ba3ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cGFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
     price: "₹550",
   },
   {
-    title: "Shoes stylish",
+    title: "Shoes (Sneaker)",
     info: "Shoes",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹700",
   },
   {
-    title: "Bahamas pants",
+    title: "Formal pants",
     info: "Pant",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1584865288642-42078afe6942?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBhbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹550",
   },
   {
-    title: "Bahamas pants",
+    title: "Pants",
     info: "Mens Pant",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1624378440847-4a64ee1a889d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHBhbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹550",
   },
   {
-    title: "Bahamas",
+    title: "T-shirt",
     info: "Mens t-shirt",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
-    price: "₹300",
+      "https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80",
+    price: "₹350",
   },
   {
-    title: "Shoes stylish",
+    title: "Canvas Shoes",
     info: "Mens Shoes",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹300",
   },
   {
-    title: "Shoes stylish",
+    title: "Nike Shoes",
     info: "Shoes",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹700",
   },
   {
-    title: "Lady skirt",
+    title: "Women skirt",
     info: "skirt lady",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1583496661160-fb5886a0aaaa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8U2tpcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹300",
   },
   {
-    title: "Bahamas",
+    title: "Black T-shirt",
     info: "t-shirt",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1503341504253-dff4815485f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
     price: "₹300",
   },
   {
     title: "Lady skirt",
     info: "skirt lady",
     imgSrc:
-      "https://images.pexels.com/photos/1261422/pexels-photo-1261422.jpeg",
+      "https://images.unsplash.com/photo-1582142306909-195724d33ffc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8U2tpcnR8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
     price: "₹300",
   },
 ];
@@ -104,7 +104,6 @@ const search = () => {
 
   for (var i = 0; i < pname.length; i++) {
     let match = productCard[i].getElementsByTagName("h2")[0];
-
     if (match) {
       let textValue = match.textContent || match.innerHTML;
 
